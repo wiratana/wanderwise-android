ada39ae1a376257f109a30c500745d41526b35f8
Alex Bramartha <91941480+alexbramartha1@users.noreply.github.com>
2023-12-28 01:11:52 +0800

    Add Notification, Crime Card, Disease Card, and Disaster Card

---
+++ app/src/main/java/com/example/wanderwise/ui/detailcity/news/DisasterCategoryDetail.kt
@@ -0,0 +1,12 @@
+package com.example.wanderwise.ui.detailcity.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wanderwise.R
+
+class DisasterCategoryDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disaster_category_detail)
+    }
+}
--- app/src/main/java/com/example/wanderwise/ui/home/notification/NotificationActivity.kt
+++ app/src/main/java/com/example/wanderwise/ui/home/notification/NotificationActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wanderwise.ui.home.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -56,6 +64,10 @@ class NotificationActivity : AppCompatActivity() {
                         )
                     }
 
+                    notifications.forEach() {
+                        sendNotification(it.subject.toString(), it.message.toString())
+                    }
+
                     val notificationAdapter = NotificationAdapter(notifications)
                     binding.rvNotification.layoutManager = LinearLayoutManager(this@NotificationActivity)
                     binding.rvNotification.setHasFixedSize(true)
@@ -68,5 +80,43 @@ class NotificationActivity : AppCompatActivity() {
             }
             refNotification.addValueEventListener(notificationListener)
         }
+    }
+
+    private fun sendNotification(title: String, message: String) {
+        val notifDetailIntent = Intent(this, NotificationActivity::class.java)
+
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(notifDetailIntent)
+            getPendingIntent(NOTIFICATION_ID, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_notifications_active)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            builder.setChannelId(CHANNEL_ID)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
+        private const val CHANNEL_NAME = "dicoding channel"
     }
 }
